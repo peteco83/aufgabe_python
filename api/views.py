2a04101ebc3c12e2a7bae7b2fc4e455469da5d4b
peteco83 <peteco1983@outlook.com>
2021-12-06 22:52:31 +0100

    Finished

---
+++ api/views.py
@@ -0,0 +1,34 @@
+from flask import Blueprint, jsonify, request, render_template
+from . import db
+from .models import Movie
+
+main = Blueprint('main', __name__)
+
+@main.route('/', methods=['GET'])
+
+def hello_world():
+
+    return render_template('index.html')
+
+@main.route('/add_movie', methods=['POST'])
+
+def add_movie():
+    movie_data = request.get_json()
+
+    new_movie = Movie(title=movie_data['title'], rating=movie_data['rating'])
+
+    db.session.add(new_movie)
+    db.session.commit()
+
+    return 'Done', 201
+
+@main.route('/movies')
+
+def movies():
+    movie_list= Movie.query.all()
+    movies= []
+
+    for movie in movie_list:
+        movies.append({'title': movie.title, 'rating': movie.rating})
+
+    return jsonify({'movies': movies})
